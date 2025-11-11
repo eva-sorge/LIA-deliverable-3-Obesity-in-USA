585ee8269f23a5fb595c1b511ea6d4fd72e9b0c0
eva-sorge <eva.sorge9@gmail.com>
2025-11-11 08:30:33 -0500

    Update LIA deliverable 3.py

--- LIA deliverable 3.py
+++ LIA deliverable 3.py
@@ -18,14 +18,21 @@ data = pd.read_csv('obesity_data.csv')
 #a): 
 data.head(10)
 data.tail(10)
+#These functions show the first and last ten entries of our data
 print(data.shape)
+#The data shape is 1000 entries through 7 columns
 data.info()
+#This function provides insight on the category names, the amount of non-null values for each category, the types of variables and the amount of variables
 data.describe()
+#This function shows the amount of variables for each category, and provides the mean, standard dev., minimum, maximum and 25%, 50% and 75% quartiles
 data.size
+#This shows the amount of values we have in our data (we can see that there are values for everything)
 data.dtypes
+#This function orovides insight on the types of values for each category
 
 #b):
 data.duplicated()
 data.drop_duplicates()  #this is the data we can use to plot graphs, to make them more visually pleasing and less confusing
 
 #c):
+print(data.isnull())
